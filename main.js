cb3af2f3377d951b4db7209739d78b91f98c2365
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-10-11 13:22:29 +0100

    get api working and get data for team

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
+const baseUrl =
+  "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple";
 
-document.getElementById('submit-btn').onclick = function() {
-    // Get the user's answer
-    var answer = document.getElementById('answer').value.trim().toLowerCase();
-  
-    // Define the correct answer in lower case
-    var correctAnswer = "paris";
-  
-    // Check the user's answer against the correct answer
-    if(answer === correctAnswer) {
-      // If correct, display success message
-      document.getElementById('result').textContent = "Correct! 🎉";
-      document.getElementById('result').style.color = "green";
-    } else {
-      // If incorrect, display failure message
-      document.getElementById('result').textContent = "Sorry, that's not correct. 😢";
-      document.getElementById('result').style.color = "red";
-    }
-  }
+const displayQuestionsAndAnswers = (contentsList) => {
+  return contentsList;
+};
+
+async function getAPIContents() {
+  const response = await axios.get(baseUrl);
+  const contentsList = response.data.results;
+  console.log(contentsList);
+  // return contentsList;
+}
+
+getAPIContents();
+
+/*
+  contentsList.forEach((question) => {
+      // store each question gotten in a variable
+      const response = question.question;
+      // console.log(response);
+      // questionsList.push(response);
+      for (let index = 0; index < contentsList.length; index++) {
+        const question = contentsList[index];
+        // console.log(question);
+      }
+      // console.log(questionsList);
+      // append to the html through the dom
+*/
+
+/*
+// get all answers in an array shuffle them with a shuffle function
+    // 1. make an array to hold the answers
+    // 2. add correct answer to the array (.push)
+      // const correctAnswersList = question.correct_answer;
+    // 3. add the incorrect answer to the array
+        // const incorrectAnswersList = question.incorrect_answers;
+        // answersList.push(correctAnswersList);
+        // answersList.push(incorrectAnswersList);
+    // 4. shuflle the array
+        // answerShuffle(answersList);
+    // 5. loop over the array and add to the dom
+*/
+
+// shuffle function
+// const answerShuffle = (answersList) => {
+//   let currentIndex = answersList.length,
+//     randomIndex;
+//   while (currentIndex > 0) {
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
+
+//     [answersList[currentIndex], answersList[randomIndex]] = [
+//       answersList[randomIndex],
+//       answersList[currentIndex],
+//     ];
+//   }
+//   return answersList;
+// };
